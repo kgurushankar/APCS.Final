0a8e342b465a80a9b90f07243b4704a3f314e53e
Unknown <keshavgurushankar@hotmail.com>
2018-05-02 09:00:22 -0700

    Random generation works well with larger maps

---
+++ src/Map.java
@@ -0,0 +1,75 @@
+import java.awt.Color;
+
+import processing.core.PApplet;
+
+public class Map {
+	/** isWall */
+	private boolean[][] state;
+
+	public Map(boolean[][] state) {
+		this.state = state;
+	}
+
+	public String toString() {
+		StringBuffer s = new StringBuffer();
+		for (boolean[] i : state) {
+			for (boolean j : i) {
+				s.append((j) ? 1 : 0);
+				s.append(' ');
+			}
+			s.append('\n');
+		}
+		return s.toString();
+	}
+
+	/**
+	 * Renders a preview of the map
+	 *
+	 * @param applet
+	 *            The PApplet used for drawing.
+	 * @param x
+	 *            The x pixel coordinate of the upper left corner of the grid
+	 *            drawing.
+	 * @param y
+	 *            The y pixel coordinate of the upper left corner of the grid
+	 *            drawing.
+	 * @param width
+	 *            The pixel width of the grid drawing.
+	 * @param height
+	 *            The pixel height of the grid drawing.
+	 */
+	public void renderPreview(PApplet applet, float x, float y, float width, float height) {
+
+		float px = x;
+		float py = y;
+		float ix = width / state.length;
+		float iy;
+		for (int i = 0; i < state.length; i++) {
+			iy = height / state[i].length;
+			for (int j = 0; j < state[i].length; j++) {
+				if (!state[j][i]) {
+					applet.fill(Color.WHITE.getRGB());
+				} else {
+					applet.fill(Color.BLACK.getRGB());
+
+				}
+				applet.rect(px, py, ix, iy);
+				px += ix;
+			}
+			py += iy;
+			px = x;
+		}
+	}
+
+	// use some kind of renderdistance variable to find optimal size of maps
+	public Map getSection(int x, int y, int width, int height) {
+		boolean[][] n = new boolean[height][width];
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				n[i][j] = state[x + i][y + j];
+			}
+		}
+		return new Map(n);
+	}
+
+}
---
+++ src/MapGenerator.java
@@ -0,0 +1,99 @@
+
+public class MapGenerator {
+	/**
+	 * This method works better when the map is larger, but as of now cannot exceed
+	 * 100*100, probably should break this down into smaller segments and generate
+	 * separately
+	 * 
+	 * @param width
+	 * @param height
+	 * @return a randomly generated map
+	 */
+	public static Map generateMap(int sideLength) {
+		if (sideLength < 100) {
+			return new Map(change(generateMapPart(sideLength, sideLength)));
+		} else {
+			int[][] state = new int[sideLength][sideLength];
+			int subcomponents = (sideLength + 99) / 100; // round up always
+			for (int i = 0; i < subcomponents; i++) {
+				for (int j = 0; j < subcomponents; j++) {
+					int[][] a = generateMapPart(100, 100);
+					for (int k = i * 100; k < (i + 1) * 100 && k < sideLength; k++) {
+						for (int l = j * 100; l < (j + 1) * 100 && l < sideLength; l++) {
+							state[k][l] = a[k - i * 100][l - j * 100];
+						}
+					}
+				}
+			}
+			// TODO check seams...
+
+			return new Map(change(state));
+		}
+	}
+
+	private static int[][] generateMapPart(int width, int height) {
+
+		int[][] state = new int[height][width];
+		int obstacles = (int) (Math.random() * height * width / 8d + height * width / 4d);// tune this properly
+		for (int i = 0; i < obstacles; i++) {
+			int a = (int) (Math.random() * height);
+			int b = (int) (Math.random() * width);
+			if (state[a][b] == 0) {
+				state[a][b] = -1;// wall
+			} else {
+				i--;
+			}
+		}
+
+		// Do DFS
+		int c = 2;
+		int val = 3;
+		int max = -1;
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				if (state[i][j] == 0) {
+					int x = DFS(state, i, j, c);
+					if (x > max) {
+						val = c;
+						max = x;
+					}
+					c++;
+				}
+			}
+		}
+		// Fill
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				if (state[i][j] == val) {
+					state[i][j] = 0;
+				} else if (state[i][j] != -1) {
+					state[i][j] = -1;
+				}
+			}
+		}
+		return state;
+	}
+
+	private static int DFS(int[][] state, int a, int b, int val) {
+		if (a >= state.length || a < 0 || b >= state[a].length || b < 0 || state[a][b] == -1) { // wall
+			return 0;
+		} else if (state[a][b] != 0) { // another things set
+			return 0;
+		} else {
+			state[a][b] = val;
+			return 1 + DFS(state, a + 1, b, val) + DFS(state, a - 1, b, val) + DFS(state, a, b + 1, val)
+					+ DFS(state, a, b - 1, val);
+		}
+	}
+
+	private static boolean[][] change(int[][] state) {
+		boolean[][] out = new boolean[state.length][];
+		for (int i = 0; i < state.length; i++) {
+			out[i] = new boolean[state[i].length];
+			for (int j = 0; j < state[i].length; j++) {
+				out[i][j] = state[i][j] == -1;
+			}
+		}
+		return out;
+	}
+}

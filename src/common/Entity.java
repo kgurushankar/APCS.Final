f820389b6f25828d0e614b17c4be0047dc2ed806
jscot <jscot@10.0.0.227>
2018-05-06 21:36:33 -0700

    here's the entity skeleton. the way to access what you want to send currently is a toString with the format x,xVelocity,y,yVelocity,identifier(which currently is a byte for flexibility but easily could become a boolean for space saving, and each piece is separated by a single space)

---
+++ src/common/Entity.java
@@ -0,0 +1,33 @@
+package common;
+
+public abstract class Entity {
+
+	protected int x,y;
+	protected double velocityX,velocityY;
+	protected byte identifier;
+	public Entity(int x, int y, double velocityX, double velocityY, byte identifier) 
+	{
+		this.x = x;
+		this.y = y;
+		this.velocityX = velocityX;
+		this.velocityY = velocityY;
+		this.identifier = identifier;
+	}
+	public void setLocation(int x1, int y1) 
+	{
+		x=x1;
+		y=y1;
+	}
+	public void addToLocation(int x, int y) 
+	{
+		this.x +=x;
+		this.y+=y;
+	}
+	
+	public abstract void act();
+	public String toString() 
+	{
+		return(x + " " + velocityX + " " + y + " " + velocityY + " " + identifier);
+	}
+
+}

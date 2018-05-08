f92d27ed40ecbc687852629a46b97af1ae834320
Unknown <keshavgurushankar@hotmail.com>
2018-05-08 11:19:50 -0700

    Accidentally broke projectile. Fixed that

---
+++ src/common/Projectile.java
@@ -0,0 +1,21 @@
+package common;
+
+public class Projectile extends Entity {
+
+	public Projectile(int x, int y, double velocityX, double velocityY, byte identifier) {
+		super(x, y, velocityX, velocityY, identifier);
+
+	}
+
+	public void act(Map m) {
+		int vel = (int) velocityY;
+		String map = m.toString();
+		for (int i = 0; i < vel; i++) {
+			if (m.canGo(x, y + 1)) {
+				y++;
+			} else {
+				break;
+			}
+		}
+	}
+}
